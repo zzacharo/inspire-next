a1a4a3e0baffbf5f25db53ddf20d28d43d476fba
Samuele Kaplun <samuele.kaplun@cern.ch>
2016-09-05 22:23:19 +0200

    travis: Integrating with Sauce Lab
    Signed-off-by: Samuele Kaplun <samuele.kaplun@cern.ch>

---
+++ tests/web/conftest.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of INSPIRE.
+# Copyright (C) 2016 CERN.
+#
+# INSPIRE is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# INSPIRE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with INSPIRE. If not, see <http://www.gnu.org/licenses/>.
+#
+# In applying this licence, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization
+# or submit itself to any jurisdiction.
+
+"""Pytest configuration for web tests."""
+
+from __future__ import absolute_import, division, print_function
+
+from os import environ
+from time import sleep
+
+import pytest
+
+from selenium import webdriver
+
+@pytest.yield_fixture(scope='session')
+def driver():
+    """Selenium driver."""
+
+    desired_cap = desired_cap = {
+        'platform': "Mac OS X 10.11",
+        'browserName': "firefox",
+        'version': "48",
+        'tunnel-identifier': environ['TRAVIS_JOB_NUMBER']
+    }
+
+    driver = webdriver.Remote(
+        command_executor='http://{SAUCE_USERNAME}:{SAUCE_ACCESSKEY}@ondemand.saucelabs.com:80/wd/hub'.format(environ),
+        desired_capabilities=desired_cap)
+
+    driver.implicitly_wait(10)
+    driver.get("http://localhost:5000")
+
+    yield driver
